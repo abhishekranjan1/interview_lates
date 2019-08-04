a635312b05e6aafa726df3b663d5388bdce4ad37
12178 <12178@DESKTOP-3BU1L2A>
2019-08-04 01:51:07 -0400

    Knapsack fixed

--- interview/src/com/abhishek/java/dynamicprogramming/KnapSack.java
+++ interview/src/com/abhishek/java/dynamicprogramming/KnapSack.java
@@ -3,38 +3,29 @@ package com.abhishek.java.dynamicprogramming;
 public class KnapSack {
 
 	public static void main(String[] args) {
-		 int[] wt = {1,3,4,5};
-		 int[] val = {1,4,5,7};
+		 int[] wt =  {0,1,3,4,5};
+		 int[] val = {0,1,4,5,7};
 		 int W=7;
 		 System.out.println(countKnapsackValue(wt,val,W));
-		
-
 	}
 
 	private static int countKnapsackValue(int[] wt, int[] val, int W) {
-		int n= wt.length;
+		int N= wt.length;
 		
-		int[][] K= new int[n+1][W+1];
-		for(int i=0;i<=n;i++)
+		int[][] K= new int[N+1][W+1];
+		for(int i=0;i < N;i++)
 		{
 			for(int w=0;w<=W;w++)
 			{
-				if(i==0||w==0)
-				{
-					K[i][w]=0;
-				}
-				else if(wt[i-1]<=w)
-				{
-					K[i][w]=Integer.max(val[i-1]+K[i-1][w-wt[i-1]],K[i-1][w]);
-				}
-				
-				else
-				{
-					K[i][w]=K[i-1][w];
-				}
+			 if(i==0||w==0)   K[i][w]=0;
+			 else {
+				 if(wt[i]<=w)    K[i][w]=Integer.max(val[i]+K[i-1][w-wt[i]],K[i-1][w]);
+				 
+				 else   K[i][w]=K[i-1][w]; 
+			 }
 			}
 		}
-		return K[n][W];
+		return K[N-1][W];
 	}
 		
 
